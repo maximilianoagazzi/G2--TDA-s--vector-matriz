376dd2f2cbd2d7a37e8397612f7b7d0f26125942
Maxi Agazzi <maximilianoagazzi2@gmail.com>
2025-08-31 13:25:48 -0300

    Ej_2 set random vector of int

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib/vector.h"
+#include "myheader.h"
 
 int main () 
 {
-
+    vector* v = set_random_vector(100);
+    for (int i=0; i<vector_size(v); i++) {
+        printf("%d ", *(int*)vector_get(v, i));
+    }
     return 0;
+}
+
+vector* set_random_vector(int size)
+{
+    vector *v = vector_new_with(size);
+    for (int i=0; i<size; i++) {
+        int *num = (int*)malloc(sizeof(int));
+        *num = rand() % 100; // Números aleatorios entre 0 y 99
+        vector_add(v, num);
+    }
+    return v;
 }
---
+++ myheader.h
@@ -0,0 +1,7 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+#include "lib/vector.h"
+
+vector* set_random_vector(int size);
+
+#endif
