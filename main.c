ba124104955d2b4c6cbf3741ffb553d0564a2d18
Maxi Agazzi <maximilianoagazzi2@gmail.com>
2025-09-01 20:23:05 -0300

    Ej_3 vector de 100 fracciones

--- main.c
+++ main.c
@@ -6,9 +6,14 @@
 
 int main () 
 {
-    vector* v = set_random_vector_int(100);
-    for (int i=0; i<vector_size(v); i++) {
-        printf("%d ", *(int*)vector_get(v, i));
+    vector* v_int = set_random_vector_int(100);
+    for (int i=0; i<vector_size(v_int); i++) {
+        printf("%d ", *(int*)vector_get(v_int, i));
+    }
+    printf("\n");
+    vector* v_frc = set_random_vector_frc(100);
+    for (int i=0; i<vector_size(v_frc); i++) {
+        fraction_print((fraction*)vector_get(v_frc, i));
     }
     return 0;
 }
@@ -28,8 +33,7 @@ vector* set_random_vector_frc(int size)
 {
     vector *v = vector_new_with(size);
     for (int i=0; i<size; i++) {
-        Fraction *num = (int*)malloc(sizeof(int));
-        *num = rand() % 100; // Números aleatorios entre 0 y 99
+        fraction *num = fraction_new(rand() % 100, (rand() % 99) + 1); // Denominador no puede ser cero
         vector_add(v, num);
     }
     return v;
--- myheader.h
+++ myheader.h
@@ -1,6 +1,6 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
-#include "lib/vector.h"
+#include "include/vector.h"
 
 vector* set_random_vector_int(int size);
 vector* set_random_vector_frc(int size);
